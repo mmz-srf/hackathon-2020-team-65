f6e8c8336486e82aee3bbbbea061a97380c0b3e8
Roman Hatz SRF <roman.hatz@srf.ch>
2020-04-04 23:39:44 +0200

    mock of timline npi chart

--- main.js
+++ main.js
@@ -3,14 +3,24 @@ const dataUrl =
 
 // replace with a Ring class for all diagrams later..
 let GOOGLE_CHARTS = [];
- 
 
 class Diagram {
-  constructor(domNode, data) {
+  constructor(domNode, data, options = {}) {
     this.domNode = domNode;
-    this.data = data; 
+    this.data = data;
+    this.type = options && options.type ? options.type : "annotatedtimeline";
+    this.googleChart = null;
   }
   render() {
+    if (this.type === "annotatedtimeline") {
+      this.renderAnnotatedtimeline();
+    } else if (this.type === "timeline") {
+      this.renderTimeline();
+    } else {
+      throw "invalid chart type" + this.type;
+    }
+  }
+  renderAnnotatedtimeline() {
     let data = new google.visualization.DataTable();
     data.addColumn("date", "Date");
     data.addColumn("number", "New Cases");
@@ -26,21 +36,46 @@ class Diagram {
       console.log("dates " + this.data[day][1]);
       data.addRows([
         [
-          new Date(Number(res[2]), res[1]-1, res[0]),
+          new Date(Number(res[2]), res[1] - 1, res[0]),
           Number(this.data[day][1]),
           undefined,
           undefined,
           Number(this.data[day][3]),
           undefined,
           undefined,
-        ]
+        ],
       ]);
-}
-    let googleChart = new google.visualization.AnnotatedTimeLine(
+    }
+
+    this.googleChart = new google.visualization.AnnotatedTimeLine(
       document.getElementById(this.domNode)
     );
-    googleChart.draw(data, { 'title':'Team 65', displayAnnotations: true, height: "100%"});  
-    return googleChart; 
+    this.googleChart.draw(data, {
+      title: "Team 65",
+      displayAnnotations: true,
+    });
+    return this.googleChart;
+  }
+  renderTimeline() {
+    let chart = new google.visualization.Timeline(document.getElementById(this.domNode));
+    let dataTable = new google.visualization.DataTable();
+
+    dataTable.addColumn({ type: 'string', id: 'Term' });
+    dataTable.addColumn({ type: 'string', id: 'Name' });
+    dataTable.addColumn({ type: 'date', id: 'Start' });
+    dataTable.addColumn({ type: 'date', id: 'End' });
+    let timetableDate = [];
+    // parse measures start/end 
+    this.data.y.forEach( (npi, index)=>{
+      console.log(npi, index)
+    })
+   
+    dataTable.addRows([
+      [ '1', 'Total shudown', new Date(1788, 3, 30), new Date(1799, 2, 9) ],
+      [ '2', 'Schools closed',new Date(1781, 3, 30),  new Date(1801, 2, 4) ],
+      [ '3', 'Travel Ban',  new Date(1789, 3, 30),  new Date(1809, 2, 4) ]]);
+
+    chart.draw(dataTable);
   }
 }
 
@@ -58,26 +93,47 @@ function loadSpreadsheet() {
 }
 
 function initGoogleCharts(data) {
-  google.charts.load("current", { packages: ["annotatedtimeline"] });
-  google.charts.setOnLoadCallback( () => {
+  google.charts.load("current", {
+    packages: ["annotatedtimeline", "timeline"],
+  });
+  google.charts.setOnLoadCallback(() => {
     renderDiagrams(data);
-  })
+  });
 }
 
 function renderDiagrams(data) {
-  GOOGLE_CHARTS.push( new Diagram("chart_div", data));
-  GOOGLE_CHARTS.forEach( (chart)=> {chart.render()}) 
+  GOOGLE_CHARTS.push(new Diagram("chart_div", data));
+
+  let npiChartData = ["NPI1", "NPI1", "NPI1", "NPI1", "NPI2", "NPI2"];
+  GOOGLE_CHARTS.push(
+    new Diagram(
+      "chart_div2",
+      { x: fetchDatesFromCsv(data), y: npiChartData },
+      { type: "timeline" }
+    )
+  );
+  GOOGLE_CHARTS.forEach((chart) => {
+    chart.render();
+  });
 }
 
-
+function fetchDatesFromCsv(data) {
+  let dates = [];
+  data.forEach((d) => {
+    if (d[0] && d[0] !== "Date") dates.push(d[0]);
+  });
+  return dates;
+}
 
 function init() {
   loadSpreadsheet().then((data) => {
     console.log("data from spreadsheet " + dataUrl, data);
-    initGoogleCharts(data); 
-   
+    initGoogleCharts(data);
+
     window.addEventListener("resize", () => {
-      GOOGLE_CHARTS.forEach( (chart)=> {chart.render()}) 
+      GOOGLE_CHARTS.forEach((chart) => {
+        chart.render();
+      });
     });
   });
 }
