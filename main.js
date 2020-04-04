5c5d2657ab102778a8ee7661657f129d58686425
Tobias <tpersson@cern.ch>
2020-04-04 17:48:35 +0200

    Just to have something to show..

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const dataUrl =  "https://docs.google.com/spreadsheets/d/1u25WTYj1JuCeIWqRSnI1FMmMfqvCcxtxHqZsFOZYptg/export?format=csv";
+const dataUrl =  "  https://docs.google.com/spreadsheets/d/1u25WTYj1JuCeIWqRSnI1FMmMfqvCcxtxHqZsFOZYptg/export?format=csv";
 
 function loadSpreadsheet() {
   return fetch(
@@ -31,19 +31,19 @@ function drawDiagram(spreadsheetData) {
         console.log("values "+Number(spreadsheetData[0]) );
         var data = new google.visualization.DataTable();
         data.addColumn('date', 'Date');
-        data.addColumn('number', 'Sold Pencils');
+        data.addColumn('number', 'New Cases');
         data.addColumn('string', 'title1');
         data.addColumn('string', 'text1');
-        data.addColumn('number', 'Sold Pens');
+        data.addColumn('number', 'Some Metric');
         data.addColumn('string', 'title2');
         data.addColumn('string', 'text2');
         data.addRows([
-          [new Date(2008, 1 ,1), Number(spreadsheetData[0]), undefined, undefined, 40645, undefined, undefined],
-          [new Date(2008, 1 ,2), 14045, undefined, undefined, 20374, undefined, undefined],
-          [new Date(2008, 1 ,3), 55022, undefined, undefined, 50766, undefined, undefined],
-          [new Date(2008, 1 ,4), 75284, undefined, undefined, 14334, 'Out of Stock','Ran out of stock on pens at 4pm'],
-          [new Date(2008, 1 ,5), 41476, 'Bought Pens','Bought 200k pens', 66467, undefined, undefined],
-          [new Date(2008, 1 ,6), 33322, undefined, undefined, 39463, undefined, undefined]
+          [new Date(2020, 3 ,21), Number(spreadsheetData[0]), undefined, undefined, undefined, undefined, undefined],
+          [new Date(2020, 3 ,22), Number(spreadsheetData[1]), undefined, undefined, undefined, undefined, undefined],
+          [new Date(2020, 3 ,23), Number(spreadsheetData[2]), undefined, undefined, undefined, undefined, undefined],
+          [new Date(2020, 3 ,24), Number(spreadsheetData[3]), undefined, undefined, undefined, 'Closed Schools','Extra information'],
+          [new Date(2020, 3 ,25), Number(spreadsheetData[4]), 'Closed shops','information', undefined, undefined, undefined],
+          [new Date(2020, 3 ,26), Number(spreadsheetData[5]), undefined, undefined, undefined, undefined, undefined]
         ]);
 
         var chart = new google.visualization.AnnotatedTimeLine(document.getElementById('chart_div'));
