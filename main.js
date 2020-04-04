df2628fd6afe0a95fadc6683d4a88af95a03aada
Roman Hatz SRF <roman.hatz@srf.ch>
2020-04-04 22:24:16 +0200

    some structure, make it possible to plot multiple diagrams

--- main.js
+++ main.js
@@ -1,6 +1,47 @@
 const dataUrl =
   "https://docs.google.com/spreadsheets/d/1R_QKcbUc5h2oBdMd4dkanDsOjyrgZsgQSQGOUCFqQ58/export?format=csv";
 
+// replace with a Ring class for all diagrams later..
+let CHARTS = [];
+ 
+
+class Diagram {
+  constructor(domNode, data) {
+    this.domNode = domNode;
+    this.data = data; 
+  }
+  render() {
+    let data = new google.visualization.DataTable();
+    data.addColumn("date", "Date");
+    data.addColumn("number", "New Cases");
+    data.addColumn("string", "title1");
+    data.addColumn("string", "text1");
+    data.addColumn("number", "Some Metric");
+    data.addColumn("string", "title2");
+    data.addColumn("string", "text2");
+    for (let day = 1; day < 10; day++) {
+    var res = this.data[day][0].split("-");
+     console.log("dates " + this.data[day][1]);
+    data.addRows([
+      [
+        new Date(Number(res[0]), res[1]-1, res[2]),
+        Number(this.data[day][1]),
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+      ]
+    ]);
+}
+    let chart = new google.visualization.AnnotatedTimeLine(
+      document.getElementById(this.domNode)
+    );
+    chart.draw(data, { displayAnnotations: true });
+    return chart; 
+  }
+}
+
 function loadSpreadsheet() {
   return fetch(dataUrl)
     .then((response) => {
@@ -14,51 +55,28 @@ function loadSpreadsheet() {
     });
 }
 
-function drawDiagram(spreadsheetData) {
-  // Load the Visualization API and the corechart package.
+function initGoogleCharts(data) {
   google.charts.load("current", { packages: ["annotatedtimeline"] });
-  google.charts.setOnLoadCallback(drawChart);
-
-  // Set a callback to run when the Google Visualization API is loaded.
-
-  function drawChart() {
-    console.log("values " + Number(spreadsheetData[0]));
-    var data = new google.visualization.DataTable();
-    data.addColumn("date", "Date");
-    data.addColumn("number", "New Cases");
-    data.addColumn("string", "title1");
-    data.addColumn("string", "text1");
-    data.addColumn("number", "Some Metric");
-    data.addColumn("string", "title2");
-    data.addColumn("string", "text2");
-    for (let day = 1; day < 10; day++) {
-    var res = spreadsheetData[day][0].split("-");
-     console.log("dates " + spreadsheetData[day][1]);
-    data.addRows([
-      [
-        new Date(Number(res[0]), res[1]-1, res[2]),
-        Number(spreadsheetData[day][1]),
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-      ]
-    ]);
+  google.charts.setOnLoadCallback( () => {
+    renderDiagrams(data);
+  })
 }
 
-    var chart = new google.visualization.AnnotatedTimeLine(
-      document.getElementById("chart_div")
-    );
-    chart.draw(data, { displayAnnotations: true });
-  }
+function renderDiagrams(data) {
+  CHARTS.push( new Diagram("chart_div", data));
+  CHARTS.forEach( (chart)=> {chart.render()}) 
 }
+
+
 
 function init() {
   loadSpreadsheet().then((data) => {
     console.log("data from spreadsheet " + dataUrl, data);
-    drawDiagram(data);
+    initGoogleCharts(data); 
+   
+    //window.addEventListener("resize", () => {});
   });
 }
-
-init();
+document.addEventListener("DOMContentLoaded", () => {
+  init();
+});
