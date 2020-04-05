a5f2fc149590378623a1aaaa50b05fa362db334b
Joanthan Tambe Ebot <jonathan.tambe.ebot@protonmail.com>
2020-04-05 16:07:19 +0200

    add navigations eventlisener functions and index modification

--- navigation.js
+++ navigation.js
@@ -1,1 +1,26 @@
- 
+
+let firstChart = document.getElementById('first_chart_container');
+let secondChart = document.getElementById('second_chart_container');
+let thirdChart = document.getElementById('third_chart_container');
+
+let btnChart1 = document.getElementById('btnChart1');
+let btnChart2 = document.getElementById('btnChart2');
+let btnChart3 = document.getElementById('btnChart3');
+
+btnChart1.addEventListener("click", function(){
+    firstChart.style.display = "block";
+    secondChart.style.display = "none";
+    thirdChart.style.display = "none";
+});
+
+btnChart2.addEventListener("click", function(){
+    firstChart.style.display = "none";
+    secondChart.style.display = "block";
+    thirdChart.style.display = "none";
+});
+
+btnChart3.addEventListener("click", function(){
+    firstChart.style.display = "none";
+    secondChart.style.display = "block";
+    thirdChart.style.display = "none";
+});
